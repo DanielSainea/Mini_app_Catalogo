6de93079b03e2ce2776d26414af06f0396d88cae
Sala D16 <CSalaD16@uniboyaca.edu.co>
2025-09-16 18:56:14 -0500

    finalizacion taller

---
+++ Mini_App_catalogo/src/app/data/products.ts
@@ -0,0 +1,7 @@
+import { Product } from '../models/product';
+
+export const PRODUCTS: Product[] = [
+  { id: 1, name: 'Laptop', price: 3500, image: 'https://panamericana.vtexassets.com/arquivos/ids/594533-800-auto?v=638838879079530000&width=800&height=auto&aspect=true', description: '__Laptop potente' },
+  { id: 2, name: 'Smartphone', price: 1200, image: 'https://mastecnologiapc.com/wp-content/uploads/2025/04/HONOR-X8C.webp', description: '__Teléfono moderno' },
+  { id: 3, name: 'Audífonos', price: 300, image: 'https://assets.bose.com/content/dam/cloudassets/Bose_DAM/Web/consumer_electronics/global/products/headphones/qc-headphonearn/product_silo_image/AEM_QCH_CHILLED-LILAC_PDP_ECOMM-GALLERY_IMG-1.png/jcr:content/renditions/cq5dam.web.1920.1920.png', description: '__Audio de calidad' }
+];
